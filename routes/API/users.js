4cecdae937dbdf92b9a979a897b0b169470dbb8b
TerryLun <tianweilun@yahoo.com>
2020-04-19 23:07:03 -0700

    registration started

--- routes/API/users.js
+++ routes/API/users.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator/check');
+
+const User = require('../../models/User');
+
 // @Route   POST api/users
 // @desc    register user
 // @access  Public(no token required)
@@ -14,13 +17,21 @@ router.post(
       'Please enter a password with 6 or more characters'
     ).isLength({ min: 6 }),
   ],
-  (req, res) => {
+  async (req, res) => {
     console.log('POST: users.js');
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    res.send('User route');
+
+    const { name, email, password } = req.body;
+
+    try {
+      res.send('User route');
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).send('Server error.');
+    }
   }
 );
 
