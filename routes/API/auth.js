e35136ce027faa49c16f80e743b3d2b31a3410a8
TerryLun <tianweilun@yahoo.com>
2020-04-20 23:55:49 -0700

    middleware and verify token

--- routes/API/auth.js
+++ routes/API/auth.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+
+const User = require('../../models/User');
 
 // @Route   get api/auth
 // @desc    Test route
 // @access  Public(no token required)
-router.get('/', (req, res) => res.send('Auth route'));
+router.get('/', auth, async (req, res) => {
+   console.log('GET: auth.js');
+   try {
+      const user = await User.findById(req.user.id).select('-password');
+      res.json(user);
+   } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error.');
+   }
+});
 
 module.exports = router;
