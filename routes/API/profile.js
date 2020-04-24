d6a4e4bbc771d3524b52791b519a346d96b90607
TerryLun <tianweilun@yahoo.com>
2020-04-23 22:52:12 -0700

    get profile

--- routes/API/profile.js
+++ routes/API/profile.js
@@ -1,9 +1,29 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
 
-// @Route   get api/profile
-// @desc    Test route
-// @access  Public(no token required)
-router.get('/', (req, res) => res.send('Profile route'));
+const profile = require('../../models/Profile');
+const User = require('../../models/User');
+
+// @Route   get api/profile/me
+// @desc    get current user's profile
+// @access  private
+router.get('/me', auth, async (req, res) => {
+   try {
+      const profile = await Profile.findOne({
+         user: req.user.id,
+      }).populate('user', ['name', 'avatar']);
+
+      if (!profile) {
+         return res
+            .status(400)
+            .json({ msg: 'There is no profile for this user.' });
+      }
+      res.json(profile);
+   } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+   }
+});
 
 module.exports = router;
