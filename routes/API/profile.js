ee4995129ca3115bbb0a93bfbdc844b2ae2376be
TerryLun <tianweilun@yahoo.com>
2020-05-03 23:33:38 -0700

    get all profiles and profile by user id

--- routes/API/profile.js
+++ routes/API/profile.js
@@ -10,7 +10,7 @@ const User = require('../../models/User');
 // @desc    get current user's profile
 // @access  private
 router.get('/me', auth, async (req, res) => {
-   console.log('GET: profile.js');
+   console.log('GET: api/profile/me');
    try {
       const profile = await Profile.findOne({
          user: req.user.id, // user id comes with token
@@ -39,7 +39,7 @@ router.post(
       check('skills', 'skills is required').not().isEmpty(),
    ],
    async (req, res) => {
-      console.log('POST: profile.js');
+      console.log('POST: api/profile');
       const errors = validationResult(req);
       if (!errors.isEmpty()) {
          return res.status(400).json({ errors: errors.array() });
@@ -107,4 +107,48 @@ router.post(
    }
 );
 
+// @Route   GET api/profile
+// @desc    get all profiles
+// @access  public
+router.get('/', async (req, res) => {
+   console.log('GET api/profile');
+
+   try {
+      const profiles = await Profile.find().populate('user', [
+         'name',
+         'avatar',
+      ]);
+      res.json(profiles);
+   } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error.');
+   }
+});
+
+// @Route   GET api/profile/user/:user_id
+// @desc    get profile by user ID
+// @access  public
+router.get('/user/:user_id', async (req, res) => {
+   console.log(`GET: api/profile/user/${req.params.user_id}`);
+
+   try {
+      // find user by id in query parameter
+      const profile = await Profile.findOne({
+         user: req.params.user_id,
+      }).populate('user', ['name', 'avatar']);
+
+      if (!profile) {
+         return res.status(400).json({ msg: 'Profile not found.' });
+      }
+
+      res.json(profile);
+   } catch (err) {
+      console.error(err.message);
+      // profile not found error message if user id does not exist
+      if (err.message.indexOf('Cast to ObjectId failed') !== -1) {
+         return res.status(400).json({ msg: 'Profile not found.' });
+      }
+      res.status(500).send('Server error.');
+   }
+});
 module.exports = router;
