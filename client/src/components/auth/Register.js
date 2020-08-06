5066577ef5af73b4bb11e162c69b2ae1d1f6bc09
TerryLun <tianweilun@yahoo.com>
2020-08-06 16:32:18 -0700

    added setAlert

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,13 +1,15 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import { setAlert } from "../../actions/alert";
 // import axios from 'axios';
 
-const Register = () => {
+const Register = (props) => {
    const [formData, setFormData] = useState({
-      name: '',
-      email: '',
-      password: '',
-      password2: '',
+      name: "",
+      email: "",
+      password: "",
+      password2: "",
    });
 
    const { name, email, password, password2 } = formData;
@@ -17,7 +19,7 @@ const Register = () => {
    const onSubmit = async (e) => {
       e.preventDefault();
       if (password !== password2) {
-         console.log('Passwords do not match.');
+         props.setAlert("Passwords do not match.", "danger");
       } else {
          //    const newUser = {
          //       name,
@@ -36,71 +38,71 @@ const Register = () => {
          //    } catch (err) {
          //       console.error(err.response.data);
          //    }
-         console.log('Success');
+         console.log("Success");
       }
    };
 
    return (
       <>
-         <h1 className='large text-primary'>Sign Up</h1>
-         <p className='lead'>
-            <i className='fas fa-user'></i> Create Your Account
+         <h1 className="large text-primary">Sign Up</h1>
+         <p className="lead">
+            <i className="fas fa-user"></i> Create Your Account
          </p>
          <form
-            className='form'
-            action='create-profile.html'
+            className="form"
+            action="create-profile.html"
             onSubmit={(e) => onSubmit(e)}
          >
-            <div className='form-group'>
+            <div className="form-group">
                <input
-                  type='text'
-                  placeholder='Name'
-                  name='name'
+                  type="text"
+                  placeholder="Name"
+                  name="name"
                   value={name}
                   onChange={(e) => onChange(e)}
                   required
                />
             </div>
-            <div className='form-group'>
+            <div className="form-group">
                <input
-                  type='email'
-                  placeholder='Email Address'
-                  name='email'
+                  type="email"
+                  placeholder="Email Address"
+                  name="email"
                   value={email}
                   onChange={(e) => onChange(e)}
                />
-               <small className='form-text'>
+               <small className="form-text">
                   This site uses Gravatar so if you want a profile image, use a
                   Gravatar email
                </small>
             </div>
-            <div className='form-group'>
+            <div className="form-group">
                <input
-                  type='password'
-                  placeholder='Password'
-                  name='password'
-                  minLength='6'
+                  type="password"
+                  placeholder="Password"
+                  name="password"
+                  minLength="6"
                   value={password}
                   onChange={(e) => onChange(e)}
                />
             </div>
-            <div className='form-group'>
+            <div className="form-group">
                <input
-                  type='password'
-                  placeholder='Confirm Password'
-                  name='password2'
-                  minLength='6'
+                  type="password"
+                  placeholder="Confirm Password"
+                  name="password2"
+                  minLength="6"
                   value={password2}
                   onChange={(e) => onChange(e)}
                />
             </div>
-            <input type='submit' className='btn btn-primary' value='Register' />
+            <input type="submit" className="btn btn-primary" value="Register" />
          </form>
-         <p className='my-1'>
-            Already have an account? <Link to='/login'>Sign In</Link>
+         <p className="my-1">
+            Already have an account? <Link to="/login">Sign In</Link>
          </p>
       </>
    );
 };
 
-export default Register;
+export default connect(null, { setAlert })(Register);
